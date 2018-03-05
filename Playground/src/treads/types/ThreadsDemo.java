3998b6c05420f74e89b5edee1969ac293f72b558
MartinBG <MartinBG@abv.bg>
2018-03-05 23:55:00 +0200

    tweaks

--- Playground/src/treads/types/ThreadsDemo.java
+++ Playground/src/treads/types/ThreadsDemo.java
@@ -4,17 +4,17 @@ public class ThreadsDemo {
 
     public static void main(String[] args) throws InterruptedException {
 
+        Thread threadExtender = new ThreadExtender();
+        Thread runnableImplementer = new Thread(new RunnableImplementer());
         Thread lambdaRunnable = new Thread(() -> {
             for (int i = 41; i <= 60; i++) {
                 System.out.println(i);
             }
         });
-        Thread threadExtender = new Thread(new ThreadExtender());
-        Thread runnableImplementer = new Thread(new RunnableImplementer());
 
-        lambdaRunnable.start();
         threadExtender.start();
         runnableImplementer.start();
+        lambdaRunnable.start();
 
         lambdaRunnable.join();
         threadExtender.join();
