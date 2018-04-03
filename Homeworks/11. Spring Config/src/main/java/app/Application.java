2c05d3504527714b54dbd2c877f72827f55ee33f
MartinBG <MartinBG@abv.bg>
2018-04-04 00:23:28 +0300

    Removed services package from scanBasePackages

--- Homeworks/11. Spring Config/src/main/java/app/Application.java
+++ Homeworks/11. Spring Config/src/main/java/app/Application.java
@@ -3,7 +3,7 @@ package app;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(scanBasePackages = {"controllers", "services", "config"})
+@SpringBootApplication(scanBasePackages = {"controllers", "config"})
 public class Application {
 
     public static void main(String[] args) {
