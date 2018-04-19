64d0a0bbef21ffd8a5c13b797c1ce89194fef5b8
MartinBG <MartinBG@abv.bg>
2018-04-20 01:52:48 +0300

    Formatting, added names to foreign keys

--- Final Project/mobilebg.sql
+++ Final Project/mobilebg.sql
@@ -24,7 +24,8 @@ CREATE TABLE `models` (
     `id` INT PRIMARY KEY AUTO_INCREMENT,
     `name` VARCHAR(20),
     `brand_id` INT NOT NULL,
-    CONSTRAINT FOREIGN KEY (`brand_id`) REFERENCES `brands`(`id`)
+    CONSTRAINT `fk_models_brands` FOREIGN KEY (`brand_id`)
+        REFERENCES `brands` (`id`)
 );
 
 CREATE TABLE `categories` (
@@ -37,46 +38,58 @@ CREATE TABLE `cities` (
     `name` VARCHAR(30)
 );
 
-CREATE TABLE `extra_categories`  (
+CREATE TABLE `extras_categories`  (
     `id` INT PRIMARY KEY AUTO_INCREMENT,
     `name` VARCHAR(30)
 );
 
-CREATE TABLE `extras`  (
+CREATE TABLE `extras` (
     `id` INT PRIMARY KEY AUTO_INCREMENT,
     `name` VARCHAR(30),
     `extra_category_id` INT NOT NULL,
-    CONSTRAINT FOREIGN KEY (`extra_category_id`) REFERENCES `extra_categories`(`id`)
+    CONSTRAINT `fk_extras_extras_categories` FOREIGN KEY (`extra_category_id`)
+        REFERENCES `extras_categories` (`id`)
 );
 
-CREATE TABLE `cars_extras`  (
+CREATE TABLE `cars_extras` (
     `id` INT PRIMARY KEY AUTO_INCREMENT,
     `car_id` INT NOT NULL,
-    `extra_id` INT NOT NULL,
-    CONSTRAINT FOREIGN KEY (`extra_id`) REFERENCES `extras`(`id`)
+    `extras_id` INT NOT NULL,
+    CONSTRAINT `fk_cars_extras_extras` FOREIGN KEY (`extras_id`)
+        REFERENCES `extras` (`id`)
 );
 
 CREATE TABLE `cars` (
     `id` INT PRIMARY KEY AUTO_INCREMENT,
     `brand_id` INT NOT NULL,
     `model_id` INT NOT NULL,
-    `condition` ENUM ('NEW', 'USED', 'PARTS') NOT NULL,
+    `condition` ENUM('NEW', 'USED', 'PARTS') NOT NULL,
     `year` INT NOT NULL,
     `horse_power` INT NOT NULL,
-    `price` DECIMAL NOT NULL,
+    `price` DECIMAL(19 , 2 ) NOT NULL,
     `category_id` INT NOT NULL,
     `city_id` INT NOT NULL,
     `max_mileage` INT NOT NULL,
-    `engine_type` ENUM ('GASOLINE', 'DIESEL', 'ELECTRIC', 'HYBRIDE') NOT NULL,
+    `engine_type` ENUM('GASOLINE', 'DIESEL', 'ELECTRIC', 'HYBRIDE') NOT NULL,
     `color_id` INT NOT NULL,
-    `gearbox` ENUM ('AUTOMATIC', 'MANUAL', 'SEMI_AUTOMATIC') NOT NULL,
+    `gearbox` ENUM('AUTOMATIC', 'MANUAL', 'SEMI_AUTOMATIC') NOT NULL,
     `car_extras_id` INT NOT NULL,
-     CONSTRAINT FOREIGN KEY (`model_id`) REFERENCES `models`(`id`),
-     CONSTRAINT FOREIGN KEY (`brand_id`) REFERENCES `brands`(`id`),
-     CONSTRAINT FOREIGN KEY (`category_id`) REFERENCES `categories`(`id`),
-     CONSTRAINT FOREIGN KEY (`city_id`) REFERENCES `cities`(`id`),
-     CONSTRAINT FOREIGN KEY (`color_id`) REFERENCES `colors`(`id`),
-     CONSTRAINT FOREIGN KEY (`car_extras_id`) REFERENCES `cars_extras`(`id`)
+    CONSTRAINT `fk_cars_models` FOREIGN KEY (`model_id`)
+        REFERENCES `models` (`id`),
+    CONSTRAINT `fk_cars_brands` FOREIGN KEY (`brand_id`)
+        REFERENCES `brands` (`id`),
+    CONSTRAINT `fk_cars_categories` FOREIGN KEY (`category_id`)
+        REFERENCES `categories` (`id`),
+    CONSTRAINT `fk_cars_cities` FOREIGN KEY (`city_id`)
+        REFERENCES `cities` (`id`),
+    CONSTRAINT `fk_cars_colors` FOREIGN KEY (`color_id`)
+        REFERENCES `colors` (`id`),
+    CONSTRAINT `fk_cars_cars_extras` FOREIGN KEY (`car_extras_id`)
+        REFERENCES `cars_extras` (`id`)
 );
 
-ALTER TABLE `cars_extras` ADD CONSTRAINT FOREIGN KEY (`car_id`) REFERENCES `cars`(`id`);
+ALTER TABLE `cars_extras` 
+ADD CONSTRAINT `fk_cars_extras_cars` 
+FOREIGN KEY (`car_id`) 
+REFERENCES `cars`(`id`);
+
