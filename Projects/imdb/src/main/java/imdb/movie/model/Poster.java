bb12666233ef01d77aba65fa3e31b4f63e58cd40
MartinBG <MartinBG@abv.bg>
2018-05-24 11:58:46 +0300

    Project structure changes

--- Projects/imdb/src/main/java/imdb/movie/model/Poster.java
+++
@@ -1,9 +0,0 @@
-package imdb.movie.model;
-
-import lombok.Data;
-
-@Data
-public class Poster {
-
-    private String url;
-}
--- Projects/imdb/src/main/java/imdb/movie/model/Trailer.java
+++
@@ -1,9 +0,0 @@
-package imdb.movie.model;
-
-import lombok.Data;
-
-@Data
-public class Trailer {
-
-    private String url;
-}
