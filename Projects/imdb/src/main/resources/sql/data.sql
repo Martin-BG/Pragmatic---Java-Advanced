77ae446346e3494beb1067f0c680d330ce9cd5fa
MartinBG <MartinBG@abv.bg>
2018-05-30 09:32:55 +0300

    Fixed typos, added more sample data

--- Projects/imdb/src/main/resources/sql/data.sql
+++ Projects/imdb/src/main/resources/sql/data.sql
@@ -7,11 +7,11 @@ INSERT INTO `genres` (`name`) VALUES
   ('Sci-Fi'),
   ('Romance'),
   ('Horror'),
-  ('Mistery'),
+  ('Mystery'),
   ('Thriller'),
   ('Crime'),
   ('Animation'),
-  ('Hisory'),
+  ('History'),
   ('Documentary'),
   ('Musical'),
   ('Biography'),
@@ -22,12 +22,14 @@ INSERT INTO `genres` (`name`) VALUES
 INSERT INTO `movies` (`title`, `year`) VALUES
   ('Dunkirk', 2017),
   ('12 Strong', 2018),
-  ('The Post', 2017);
+  ('The Post', 2017),
+  ('Saving Private Ryan', 1998);
 
 INSERT INTO `movies_genres` (`movie_id`, `genre_id`) VALUES
   (1, 1), (1, 3), (1, 13),
   (2, 1), (2, 3), (2, 13),
-  (3, 16), (3, 3), (3, 13);
+  (3, 16), (3, 3), (3, 13),
+  (4, 3), (4, 13), (4, 17);
 
 INSERT INTO `actors` (`name`) VALUES
   ('Chris Hemsworth'),
@@ -38,20 +40,32 @@ INSERT INTO `actors` (`name`) VALUES
   ('Mark Rylance'),
   ('Meryl Streep'),
   ('Tom Hanks'),
-  ('Sarah Paulson');
+  ('Sarah Paulson'),
+  ('Matt Damon'),
+  ('Tom Sizemore');
 
 INSERT INTO `movies_actors` (`movie_id`, `actor_id`) VALUES
   (1, 4), (1, 5), (1, 6),
   (2, 1), (2, 2), (2, 3),
-  (3, 7), (3, 8), (3, 9);
+  (3, 7), (3, 8), (3, 9),
+  (4, 8), (4, 10), (4, 11);
 
 INSERT INTO `users` (`email`, `password`) VALUES
   ('pesho@abv.bg', 'pesho'),
   ('gosho@abv.bg', 'gosho');
 
 INSERT INTO `movies_user_ratings` (`movie_id`, `user_id`, `rating`) VALUES
-  (1, 1, 5), (2, 1, 7), (3, 1, 4),
-  (1, 2, 10), (2, 2, 9), (3, 2, 6);
+  (1, 1, 5), (2, 1, 7), (3, 1, 4), (4, 1, 9),
+  (1, 2, 10), (2, 2, 9), (3, 2, 6), (4, 2, 8);
 
 INSERT INTO `users_movies` (`user_id`, `movie_id`) VALUES
-  (1, 1), (2, 2), (2, 3);
+  (1, 1), (2, 2), (2, 3), (1, 4);
+
+INSERT INTO `posters` (`movie_id`, `url`) VALUES
+  (4, 'http://i0.kym-cdn.com/photos/images/original/001/254/427/32c.jpg'),
+  (3, 'http://www.impawards.com/2017/posters/post_ver2_xlg.jpg');
+
+INSERT INTO `trailers` (`movie_id`, `url`) VALUES
+  (4, 'https://www.youtube.com/watch?v=RYID71hYHzg'),
+  (4, 'https://www.youtube.com/watch?v=yiyjRA7X2hg'),
+  (3, 'https://www.youtube.com/watch?v=nrXlY6gzTTM');
