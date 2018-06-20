b160b5fe9eb40ad6e18c5fa30ad2335a62237783
MartinBG <MartinBG@abv.bg>
2018-06-20 13:57:24 +0300

    Added final modifier to MovieRowMapper.java

--- Projects/imdb/src/main/java/imdb/persistence/movie/MovieRowMapper.java
+++ Projects/imdb/src/main/java/imdb/persistence/movie/MovieRowMapper.java
@@ -6,7 +6,7 @@ import org.springframework.jdbc.core.RowMapper;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
-class MovieRowMapper implements RowMapper<Movie> {
+final class MovieRowMapper implements RowMapper<Movie> {
 
     public Movie mapRow(final ResultSet rs, final int rowNum) throws SQLException {
         return new Movie(
