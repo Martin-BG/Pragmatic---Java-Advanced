f0ec9a92d64fd2c5b53823f71f00a47ec1baacef
MartinBG <MartinBG@abv.bg>
2018-06-20 16:48:07 +0300

    Movie class collections refactoring - state protection, preserve order of elements, higher level of abstraction

--- Projects/imdb/src/main/java/imdb/model/Movie.java
+++ Projects/imdb/src/main/java/imdb/model/Movie.java
@@ -2,8 +2,8 @@ package imdb.model;
 
 import lombok.Data;
 
-import java.util.HashSet;
-import java.util.Set;
+import java.util.*;
+import java.util.stream.Collectors;
 
 @Data
 public final class Movie {
@@ -13,10 +13,10 @@ public final class Movie {
     private String owner;
     private Integer votes;
     private Double rating;
-    private Set<String> genres;
-    private Set<String> actors;
-    private Set<String> posters;
-    private Set<String> trailers;
+    private Collection<String> genres;
+    private Collection<String> actors;
+    private Collection<String> posters;
+    private Collection<String> trailers;
 
     public Movie(final String title, final int year) {
         this.title = title;
@@ -24,9 +24,58 @@ public final class Movie {
         this.owner = null;
         this.votes = 0;
         this.rating = 0.0;
-        this.genres = new HashSet<>();
-        this.actors = new HashSet<>();
-        this.posters = new HashSet<>();
-        this.trailers = new HashSet<>();
+        this.setGenres(null);
+        this.setActors(null);
+        this.setPosters(null);
+        this.setTrailers(null);
+    }
+
+    private Collection<String> toCollection(final Collection<String> entries) {
+        if (entries == null) {
+            return new LinkedHashSet<>();
+        }
+
+        return entries.stream()
+                .collect(Collectors.toMap(
+                        String::toLowerCase,    // Case insensitive distinct
+                        entry -> entry,
+                        (e1, e2) -> e1,         // Keep first entry
+                        LinkedHashMap::new))    // Preserve input order
+                .entrySet()
+                .stream()
+                .map(Map.Entry::getValue)
+                .collect(Collectors.toCollection(LinkedHashSet::new));
+    }
+
+    public Collection<String> getGenres() {
+        return Collections.unmodifiableCollection(this.genres);
+    }
+
+    public void setGenres(final Collection<String> genres) {
+        this.genres = this.toCollection(genres);
+    }
+
+    public Collection<String> getActors() {
+        return Collections.unmodifiableCollection(this.actors);
+    }
+
+    public void setActors(final Collection<String> actors) {
+        this.actors = this.toCollection(actors);
+    }
+
+    public Collection<String> getPosters() {
+        return Collections.unmodifiableCollection(this.posters);
+    }
+
+    public void setPosters(final Collection<String> posters) {
+        this.posters = this.toCollection(posters);
+    }
+
+    public Collection<String> getTrailers() {
+        return Collections.unmodifiableCollection(this.trailers);
+    }
+
+    public void setTrailers(final Collection<String> trailers) {
+        this.trailers = this.toCollection(trailers);
     }
 }
